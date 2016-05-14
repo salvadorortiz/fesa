bbf83a4920be518f7cfe759ec679f0df7494364e
salvador <salvadorortiz.91@gmail.com>
2016-05-14 08:17:14 -0600

    Cambio de Ubuntu a Mint

--- modulo_1/admin.py
+++ modulo_1/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#class MedicoAdmin(admin.ModelAdmin):
+#	list_display =('nombre','especialidad')
+#class ConsultaAdmin(admin.ModelAdmin):
+#	list_display =('fecha','paciente','medico')
+#class UsuarioAdmin(admin.ModelAdmin):
+#	list_display =('user','password','medico','paciente','estado')
+
+#admin.site.register(Medico, MedicoAdmin)
+#admin.site.register(Paciente)
+#admin.site.register(Consulta,ConsultaAdmin)
+#admin.site.register(Usuario,UsuarioAdmin)
+
+admin.site.register(Usuario)
+admin.site.register(Complejo)
+admin.site.register(HorarioComplejo)
+admin.site.register(Cancha)
+admin.site.register(Cliente)
+admin.site.register(Empresa)
+admin.site.register(FormaPago)
+admin.site.register(FormaFacturacion)
+admin.site.register(TipoAlquiler)
+admin.site.register(Reserva)
+admin.site.register(ReservaCancha)
