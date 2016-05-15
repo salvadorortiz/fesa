ead67749f07fa396ea277f832a5b101dd998c585
salvador <salvadorortiz_13@hotmail.com>
2016-05-14 19:42:08 -0600

    Inclusión de librerias CSS y JS - Estructura inicial del base.html

--- fesa/settings.py
+++ fesa/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'fesa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fesa_dev',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -101,3 +104,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"), ) 
