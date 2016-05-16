9ecc4ce974c0664624ec7d3a6bdb1b7ce5ee285d
salvador <salvadorortiz_13@hotmail.com>
2016-05-15 21:09:38 -0600

    Creación de pantalla precios.html - falta programar funcionalidad

--- modulo_1/registro_urls.py
+++ modulo_1/registro_urls.py
@@ -4,4 +4,5 @@ from modulo_1 import cliente_views
 
 urlpatterns = [
     url(r'^cliente/', 'modulo_1.cliente_views.RegistroClienteView',name='cliente_registrocliente'),
+    url(r'^precios/', 'modulo_1.views.PreciosView',name='precios_guia'),
 ]
--- modulo_1/views.py
+++ modulo_1/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def PreciosView(request):
+	return render(request,'precios.html')
